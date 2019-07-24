120fad97d2521f4c4d377f5155c096ebb6b4da00
effort <1258849627@qq.com>
2019-07-24 07:38:46 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,6 +4,6 @@
 	<h1>Hello Laravel</h1>
 	<p class="lead">你现在所看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a></p>的示例项目主页
 	<p>一切，将从这里开始。</p>
-	<p><a href="#" class="btn btn-lg btn-success" role="button">现在注册</a></p>
+	<p><a href="{{ route('signup') }}" class="btn btn-lg btn-success" role="button">现在注册</a></p>
 </div>
 @stop
